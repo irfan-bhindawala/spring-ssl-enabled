8449f0394fd7a6b2b34586c0e8c8863d15f64846
Bhindawala, Irfan <irfan.bhindawala@mastercard.com>
2018-08-04 20:14:20 +0530

    initial project initialization commit

---
+++ src/main/java/com/techzone/sslenabled/SslEnabledApplication.java
@@ -0,0 +1,12 @@
+package com.techzone.sslenabled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslEnabledApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslEnabledApplication.class, args);
+    }
+}
